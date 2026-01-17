9b689ec9463787cc04862ac7fd70e725b1778ca5
Andrew Graham <and4rewandy5@gmail.com>
2026-01-17 11:06:05 -0600

    port

--- manager/src/main/java/com/revature/Main.java
+++ manager/src/main/java/com/revature/Main.java
@@ -46,8 +46,8 @@ public class Main {
             // Enable CORS for cross-origin requests from frontend
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(it -> {
-                    it.allowHost("http://127.0.0.1:5000");
-                    it.allowHost("http://localhost:5000");
+                    it.allowHost("http://127.0.0.1:5001");
+                    it.allowHost("http://localhost:5001");
                     it.allowCredentials = true;
                 });
             });
