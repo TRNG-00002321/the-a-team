9e4db8bfb6fa9dde3543093a90728e17aa5b3bd7
hpham474 <hpham474@gmail.com>
2026-01-21 01:01:59 -0600

    add chrome options

--- employee/tests/end_to_end_test/drivers/browser_manager.py
+++ employee/tests/end_to_end_test/drivers/browser_manager.py
@@ -24,6 +24,12 @@ def create_driver(browser_name: str, headless: bool = False):
             }
         )
 
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+
+
         service = ChromeService(ChromeDriverManager().install())
         return webdriver.Chrome(service=service, options=options)
 
